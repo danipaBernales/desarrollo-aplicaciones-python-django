8eda5eff5a2a312d5a3e4fa56e66c6cf02156098
Daniela Paz Bernales <dbernales.miranda@gmail.com>
2024-06-22 23:26:59 -0400

    Delete ABPro-Proyecto_Grupal_3/telovendo/app_telovendo directory

--- ABPro-Proyecto_Grupal_3/telovendo/app_telovendo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Customer
-
-class CustomerForm(forms.ModelForm):
-    class Meta:
-        model = Customer
-        fields = ['name', 'last_name', 'email', 'birth_city', 'address', 'phone_number', 'creation_date']
--- ABPro-Proyecto_Grupal_3/telovendo/app_telovendo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('', views.customer_list, name='customer_list'),
-]
